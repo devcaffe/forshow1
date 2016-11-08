2be5213ba902438982e91433fd7ffc134d36b3d4
ruska <ruskas@yahoo.com>
2016-11-08 18:56:14 -0500

    3 commit

--- aws-app.sh
+++ aws-app.sh
@@ -9,6 +9,14 @@ source waveansible/bin/activate
 pip install ansible
 pip install boto
 
-AWS_PROFILE=wtg ansible-playbook   playbook.yml --ask-vault-pass
+#AWS_PROFILE=wtg ansible-playbook --tag aws   playbook.yml --ask-vault-pass
+AWS_PROFILE=wtg ansible-playbook --tag aws   playbook.yml 
 
-echo $foobar
+new_instance_id=$(aws ec2 describe-instances --filters Name=tag:Name,Values=waveapp --profile wtg --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+
+echo $new_instance_id
+
+ssh -i "~/.ssh/wtg-key1.pem"   ubuntu@$new_instance_id  "sudo apt-get -y update && sudo apt-get -y install python2.7" 
+
+#AWS_PROFILE=wtg ansible-playbook --tag common,nginx,deploy   playbook.yml --ask-vault-pass
+AWS_PROFILE=wtg ansible-playbook --tag common,nginx,deploy -i $new_instance_id,  playbook.yml 
